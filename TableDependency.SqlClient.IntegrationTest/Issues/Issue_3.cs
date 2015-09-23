f0fa09a7528faf14188592cc75886cd81173f1b2
christian.delbianco <christian.delbianco@gmail.com>
2015-09-23 19:08:09 +0200

    Issue 3

---
+++ TableDependency.SqlClient.IntegrationTest/Issues/Issue_3.cs
@@ -0,0 +1,96 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TableDependency.EventArgs;
+using TableDependency.Mappers;
+
+namespace TableDependency.SqlClient.IntegrationTest
+{
+    [TestClass]
+    public class Issue_3
+    {
+        private static string _connectionString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+        private const string TableName = "BiddingTextDependancy";
+
+        public TestContext TestContext { get; set; }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                using (var sqlCommand = sqlConnection.CreateCommand())
+                {
+                    sqlCommand.CommandText = "DROP TABLE BiddingTextDependancy";
+                    sqlCommand.ExecuteNonQuery();
+
+                    sqlCommand.CommandText = "CREATE TABLE BiddingTextDependancy ([id][int] IDENTITY(1, 1) NOT NULL, [full_text] [NVARCHAR](100) NOT NULL, [saleid] [NVARCHAR](100) NOT NULL)";
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ModelToTableMapper_only_seems_to_work_with_strings()
+        {
+            SqlTableDependency<BiddingTextDependancyDto> tableDependency = null;
+
+            try
+            {
+                var mapper = new ModelToTableMapper<BiddingTextDependancyDto>();
+                mapper.AddMapping(c => c.Id, "id").AddMapping(c => c.Text, "full_text").AddMapping(c => c.SaleId, "saleid");
+
+                tableDependency = new SqlTableDependency<BiddingTextDependancyDto>(_connectionString, TableName, mapper);
+                tableDependency.OnChanged += TableDependency_Changed;
+                tableDependency.Start();
+                
+                Thread.Sleep(5000);
+
+                var t = new Task(ModifyTableContent);
+                t.Start();
+                t.Wait(20000);
+            }
+            finally
+            {
+                tableDependency?.Dispose();
+            }
+        }
+
+        private void TableDependency_Changed(object sender, RecordChangedEventArgs<BiddingTextDependancyDto> e)
+        {
+            this.TestContext.WriteLine(e.ChangeType + ": " + e.Entity.Id + " " + e.Entity.Text + " " + e.Entity.SaleId);
+        }
+
+        private static void ModifyTableContent()
+        {
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+                using (var sqlCommand = sqlConnection.CreateCommand())
+                {
+                    sqlCommand.CommandText = $"INSERT INTO [dbo].[BiddingTextDependancy] ([full_text], [saleid]) VALUES ('BBBBBBBBBB', 987)";
+                    sqlCommand.ExecuteNonQuery();
+                    Thread.Sleep(1000);
+
+                    sqlCommand.CommandText = $"UPDATE [dbo].[BiddingTextDependancy] SET [full_text] = 'AAAAAAAAAAAA', [saleid] = 123";
+                    sqlCommand.ExecuteNonQuery();
+                    Thread.Sleep(1000);
+
+                    sqlCommand.CommandText = "DELETE FROM [dbo].[BiddingTextDependancy]";
+                    sqlCommand.ExecuteNonQuery();
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        public class BiddingTextDependancyDto
+        {
+            public long Id { get; set; }
+            public string Text { get; set; }
+            public int SaleId { get; set; }
+        }
+    }
+}
--- TableDependency/Mappers/ModelToTableMapper.cs
+++ TableDependency/Mappers/ModelToTableMapper.cs
@@ -7,28 +7,66 @@ using TableDependency.Exceptions;
 
 namespace TableDependency.Mappers
 {
+    /// <summary>
+    /// Model to column database table mapper.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class ModelToTableMapper<T> where T : class
     {
         private readonly IDictionary<PropertyInfo, string> _mappings = new Dictionary<PropertyInfo, string>();
 
-        public ModelToTableMapper<T> AddMapping(Expression<Func<T, object>> property, string columnName)
+        /// <summary>
+        /// Adds the mapping between a model property and a database table column, in order to decouple naming and to overcome the impossibility to map SQL columns name containing spaces.
+        /// </summary>
+        /// <param name="expression">The expression.</param>
+        /// <param name="columnName">Name of the column.</param>
+        /// <returns></returns>
+        public ModelToTableMapper<T> AddMapping(Expression<Func<T, object>> expression, string columnName)
         {
             if (string.IsNullOrWhiteSpace(columnName)) throw new ModelToTableMapperException("ModelToTableMapper cannot contains null or empty strings.");
 
-            _mappings[(PropertyInfo)((MemberExpression)property.Body).Member] = columnName;
-            return this;
+            var memberExpression = expression.Body as MemberExpression;
+            if (memberExpression != null)
+            {
+                _mappings[(PropertyInfo)memberExpression.Member] = columnName;
+                return this;
+            }
+
+            var unarUnaryExpressionyExp = expression.Body as UnaryExpression;
+            var memberExpressionByOperator = unarUnaryExpressionyExp?.Operand as MemberExpression;
+            if (memberExpressionByOperator != null)
+            {
+                _mappings[(PropertyInfo)memberExpressionByOperator.Member] = columnName;
+                return this;
+            }
+
+            throw new ArgumentException("'expression' parameter should be a member expression", nameof(expression));
         }
 
+        /// <summary>
+        /// Return number of mappings.
+        /// </summary>
+        /// <returns></returns>
         public int Count()
         {
             return _mappings.Count;
         }
 
+        /// <summary>
+        /// Given the specified model property info return the table column name associated.
+        /// </summary>
+        /// <param name="propertyInfo">The property information.</param>
+        /// <returns></returns>
         internal string GetMapping(PropertyInfo propertyInfo)
         {
             return _mappings.ContainsKey(propertyInfo) ? _mappings[propertyInfo] : null;
         }
 
+        /// <summary>
+        /// Given the specified table column name, return the model property associated.
+        /// </summary>
+        /// <param name="tableColumnName">Name of the table column.</param>
+        /// <returns></returns>
         internal string GetMapping(string tableColumnName)
         {
             if (GetMappings().Any(kvp => kvp.Value != null && string.Compare(kvp.Value, tableColumnName, StringComparison.OrdinalIgnoreCase) == 0))
