8743cfa4473654070fdf18827dd844ed5aa172f4
Christian Del Bianco <christian.delbianco@additiv.ch>
2018-04-29 23:40:50 +0200

    INTEGRATION TESTS

--- TableDependency.Tests/TableDependency.IntegrationTest/Issue53.cs
+++ TableDependency.Tests/TableDependency.IntegrationTest/Issue53.cs
@@ -31,14 +31,30 @@ namespace TableDependency.IntegrationTest
         public SexEnum Gender { get; set; }
     }
 
+    internal class Issue53Model3
+    {
+        public int Id { get; set; }
+        public SexEnum? Gender { get; set; }
+    }
+
+    internal class Issue53Model4
+    {
+        public int Id { get; set; }
+        public SexEnum Gender { get; set; }
+    }
+
     [TestClass]
     public class Issue53 : SqlTableDependencyBaseTest
     {
         public TestContext TestContext { get; set; }
         private const string TableName1 = "Issue53Model1";
         private const string TableName2 = "Issue53Model2";
+        private const string TableName3 = "Issue53Model3";
+        private const string TableName4 = "Issue53Model4";
         private static Dictionary<string, Tuple<Issue53Model1, Issue53Model1>> _checkValues1 = new Dictionary<string, Tuple<Issue53Model1, Issue53Model1>>();
         private static Dictionary<string, Tuple<Issue53Model2, Issue53Model2>> _checkValues2 = new Dictionary<string, Tuple<Issue53Model2, Issue53Model2>>();
+        private static Dictionary<string, Tuple<Issue53Model3, Issue53Model3>> _checkValues3 = new Dictionary<string, Tuple<Issue53Model3, Issue53Model3>>();
+        private static Dictionary<string, Tuple<Issue53Model4, Issue53Model4>> _checkValues4 = new Dictionary<string, Tuple<Issue53Model4, Issue53Model4>>();
 
         [ClassInitialize()]
         public static void ClassInitialize(TestContext testContext)
@@ -57,6 +73,16 @@ namespace TableDependency.IntegrationTest
                     sqlCommand.ExecuteNonQuery();
                     sqlCommand.CommandText = $"CREATE TABLE [{TableName2}]([Id] [int] NULL, [Gender] [int])";
                     sqlCommand.ExecuteNonQuery();
+
+                    sqlCommand.CommandText = $"IF OBJECT_ID('[{TableName3}]', 'U') IS NOT NULL DROP TABLE [dbo].[{TableName3}]";
+                    sqlCommand.ExecuteNonQuery();
+                    sqlCommand.CommandText = $"CREATE TABLE [{TableName3}]([Id] [int] NULL, [Gender] [int] NULL)";
+                    sqlCommand.ExecuteNonQuery();
+
+                    sqlCommand.CommandText = $"IF OBJECT_ID('[{TableName4}]', 'U') IS NOT NULL DROP TABLE [dbo].[{TableName4}]";
+                    sqlCommand.ExecuteNonQuery();
+                    sqlCommand.CommandText = $"CREATE TABLE [{TableName4}]([Id] [int] NULL, [Gender] [int] NULL)";
+                    sqlCommand.ExecuteNonQuery();
                 }
             }
         }
@@ -73,6 +99,12 @@ namespace TableDependency.IntegrationTest
                     sqlCommand.ExecuteNonQuery();
 
                     sqlCommand.CommandText = $"IF OBJECT_ID('{TableName2}', 'U') IS NOT NULL DROP TABLE [{TableName2}];";
+                    sqlCommand.ExecuteNonQuery();
+
+                    sqlCommand.CommandText = $"IF OBJECT_ID('{TableName3}', 'U') IS NOT NULL DROP TABLE [{TableName3}];";
+                    sqlCommand.ExecuteNonQuery();
+
+                    sqlCommand.CommandText = $"IF OBJECT_ID('{TableName4}', 'U') IS NOT NULL DROP TABLE [{TableName4}];";
                     sqlCommand.ExecuteNonQuery();
                 }
             }
@@ -148,6 +180,76 @@ namespace TableDependency.IntegrationTest
             Assert.IsTrue(base.CountConversationEndpoints(objectNaming) == 0);
         }
 
+        [TestCategory("SqlServer")]
+        [TestMethod]
+        public void TestTable3()
+        {
+            string objectNaming;
+            var tableDependency = new SqlTableDependency<Issue53Model3>(ConnectionStringForTestUser, TableName3);
+
+            try
+            {
+                tableDependency.OnChanged += TableDependency_Changed3;
+                tableDependency.Start();
+                objectNaming = tableDependency.DataBaseObjectsNamingConvention;
+
+                Thread.Sleep(5000);
+
+                var t = new Task(ModifyTableContent3);
+                t.Start();
+                Thread.Sleep(1000 * 10 * 1);
+            }
+            finally
+            {
+                tableDependency.Dispose();
+            }
+
+            Assert.AreEqual(_checkValues3[ChangeType.Insert.ToString()].Item2.Id, _checkValues3[ChangeType.Insert.ToString()].Item1.Id);
+            Assert.AreEqual(_checkValues3[ChangeType.Insert.ToString()].Item2.Gender, _checkValues3[ChangeType.Insert.ToString()].Item1.Gender);
+            Assert.AreEqual(_checkValues3[ChangeType.Update.ToString()].Item2.Id, _checkValues3[ChangeType.Update.ToString()].Item1.Id);
+            Assert.AreEqual(_checkValues3[ChangeType.Update.ToString()].Item2.Gender, _checkValues3[ChangeType.Update.ToString()].Item1.Gender);
+            Assert.AreEqual(_checkValues3[ChangeType.Delete.ToString()].Item2.Id, _checkValues3[ChangeType.Delete.ToString()].Item1.Id);
+            Assert.AreEqual(_checkValues3[ChangeType.Delete.ToString()].Item2.Gender, _checkValues3[ChangeType.Delete.ToString()].Item1.Gender);
+
+            Assert.IsTrue(base.AreAllDbObjectDisposed(objectNaming));
+            Assert.IsTrue(base.CountConversationEndpoints(objectNaming) == 0);
+        }
+
+        [TestCategory("SqlServer")]
+        [TestMethod]
+        public void TestTable4()
+        {
+            string objectNaming;
+            var tableDependency = new SqlTableDependency<Issue53Model4>(ConnectionStringForTestUser, TableName4);
+
+            try
+            {
+                tableDependency.OnChanged += TableDependency_Changed4;
+                tableDependency.Start();
+                objectNaming = tableDependency.DataBaseObjectsNamingConvention;
+
+                Thread.Sleep(5000);
+
+                var t = new Task(ModifyTableContent4);
+                t.Start();
+                Thread.Sleep(1000 * 10 * 1);
+            }
+            finally
+            {
+                tableDependency.Dispose();
+            }
+
+            Assert.AreEqual(_checkValues4[ChangeType.Insert.ToString()].Item2.Id, _checkValues4[ChangeType.Insert.ToString()].Item1.Id);
+            Assert.AreEqual(_checkValues4[ChangeType.Insert.ToString()].Item2.Gender, _checkValues4[ChangeType.Insert.ToString()].Item1.Gender);
+            Assert.AreEqual(_checkValues4[ChangeType.Update.ToString()].Item2.Id, _checkValues4[ChangeType.Update.ToString()].Item1.Id);
+            Assert.AreEqual(_checkValues4[ChangeType.Update.ToString()].Item2.Gender, _checkValues4[ChangeType.Update.ToString()].Item1.Gender);
+            Assert.AreEqual(_checkValues4[ChangeType.Delete.ToString()].Item2.Id, _checkValues4[ChangeType.Delete.ToString()].Item1.Id);
+            Assert.AreEqual(_checkValues4[ChangeType.Delete.ToString()].Item2.Gender, _checkValues4[ChangeType.Delete.ToString()].Item1.Gender);
+
+            Assert.IsTrue(base.AreAllDbObjectDisposed(objectNaming));
+            Assert.IsTrue(base.CountConversationEndpoints(objectNaming) == 0);
+        }
+
         private static void TableDependency_Changed1(object sender, RecordChangedEventArgs<Issue53Model1> e)
         {
             switch (e.ChangeType)
@@ -190,6 +292,48 @@ namespace TableDependency.IntegrationTest
             }
         }
 
+        private static void TableDependency_Changed3(object sender, RecordChangedEventArgs<Issue53Model3> e)
+        {
+            switch (e.ChangeType)
+            {
+                case ChangeType.Insert:
+                    _checkValues3[ChangeType.Insert.ToString()].Item2.Id = e.Entity.Id;
+                    _checkValues3[ChangeType.Insert.ToString()].Item2.Gender = e.Entity.Gender;
+                    break;
+
+                case ChangeType.Delete:
+                    _checkValues3[ChangeType.Delete.ToString()].Item2.Id = e.Entity.Id;
+                    _checkValues3[ChangeType.Delete.ToString()].Item2.Gender = e.Entity.Gender;
+                    break;
+
+                case ChangeType.Update:
+                    _checkValues3[ChangeType.Update.ToString()].Item2.Id = e.Entity.Id;
+                    _checkValues3[ChangeType.Update.ToString()].Item2.Gender = e.Entity.Gender;
+                    break;
+            }
+        }
+
+        private static void TableDependency_Changed4(object sender, RecordChangedEventArgs<Issue53Model4> e)
+        {
+            switch (e.ChangeType)
+            {
+                case ChangeType.Insert:
+                    _checkValues4[ChangeType.Insert.ToString()].Item2.Id = e.Entity.Id;
+                    _checkValues4[ChangeType.Insert.ToString()].Item2.Gender = e.Entity.Gender;
+                    break;
+
+                case ChangeType.Delete:
+                    _checkValues4[ChangeType.Delete.ToString()].Item2.Id = e.Entity.Id;
+                    _checkValues4[ChangeType.Delete.ToString()].Item2.Gender = e.Entity.Gender;
+                    break;
+
+                case ChangeType.Update:
+                    _checkValues4[ChangeType.Update.ToString()].Item2.Id = e.Entity.Id;
+                    _checkValues4[ChangeType.Update.ToString()].Item2.Gender = e.Entity.Gender;
+                    break;
+            }
+        }
+
         private static void ModifyTableContent1()
         {
             _checkValues1.Add(ChangeType.Insert.ToString(), new Tuple<Issue53Model1, Issue53Model1>(new Issue53Model1 { Id = 23, Gender = SexEnum.Female }, new Issue53Model1()));
@@ -216,6 +360,34 @@ namespace TableDependency.IntegrationTest
                 _checkValues2[ChangeType.Insert.ToString()].Item1.Gender.GetHashCode(),
                 _checkValues2[ChangeType.Update.ToString()].Item1.Id,
                 _checkValues2[ChangeType.Update.ToString()].Item1.Gender.GetHashCode());
+        }
+
+        private static void ModifyTableContent3()
+        {
+            _checkValues3.Add(ChangeType.Insert.ToString(), new Tuple<Issue53Model3, Issue53Model3>(new Issue53Model3 { Id = 7, Gender = SexEnum.Female }, new Issue53Model3()));
+            _checkValues3.Add(ChangeType.Update.ToString(), new Tuple<Issue53Model3, Issue53Model3>(new Issue53Model3 { Id = 4, Gender = SexEnum.Male }, new Issue53Model3()));
+            _checkValues3.Add(ChangeType.Delete.ToString(), new Tuple<Issue53Model3, Issue53Model3>(new Issue53Model3 { Id = 4, Gender = SexEnum.Male }, new Issue53Model3()));
+
+            ModifyTableContent(
+                TableName3,
+                _checkValues3[ChangeType.Insert.ToString()].Item1.Id,
+                _checkValues3[ChangeType.Insert.ToString()].Item1.Gender.GetHashCode(),
+                _checkValues3[ChangeType.Update.ToString()].Item1.Id,
+                _checkValues3[ChangeType.Update.ToString()].Item1.Gender.GetHashCode());
+        }
+
+        private static void ModifyTableContent4()
+        {
+            _checkValues4.Add(ChangeType.Insert.ToString(), new Tuple<Issue53Model4, Issue53Model4>(new Issue53Model4 { Id = 57, Gender = SexEnum.Female }, new Issue53Model4()));
+            _checkValues4.Add(ChangeType.Update.ToString(), new Tuple<Issue53Model4, Issue53Model4>(new Issue53Model4 { Id = 4, Gender = SexEnum.Male }, new Issue53Model4()));
+            _checkValues4.Add(ChangeType.Delete.ToString(), new Tuple<Issue53Model4, Issue53Model4>(new Issue53Model4 { Id = 4, Gender = SexEnum.Male }, new Issue53Model4()));
+
+            ModifyTableContent(
+                TableName4,
+                _checkValues4[ChangeType.Insert.ToString()].Item1.Id,
+                _checkValues4[ChangeType.Insert.ToString()].Item1.Gender.GetHashCode(),
+                _checkValues4[ChangeType.Update.ToString()].Item1.Id,
+                _checkValues4[ChangeType.Update.ToString()].Item1.Gender.GetHashCode());
         }
 
         private static void ModifyTableContent(string tableName, int idInsert, int genderInsert, int idUpdate, int genderUpdate)
