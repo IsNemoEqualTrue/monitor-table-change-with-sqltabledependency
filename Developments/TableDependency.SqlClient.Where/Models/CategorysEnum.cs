1f5fa5e85a473176027f82549d67b177fe5368f3
Christian Del Bianco <christian.delbianco@additiv.ch>
2018-08-19 20:02:53 +0200

    removed old solution items

--- Developments/TableDependency.SqlClient.Where/Models/CategorysEnum.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TableDependency.SqlClient.Where.Development.Models
-{
-    public enum CategorysEnum
-    {
-        Food = 1,
-        Drink = 2,
-        Dessert = 3
-    }
-}
--- Tests/TableDependency.SqlClient.LoadTests/ApplicationListener1/LoadTest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ApplicationListener1
-{
-    public class LoadTest
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string SecondName { get; set; }
-    }
-}
