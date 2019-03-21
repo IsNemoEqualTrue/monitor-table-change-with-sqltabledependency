16588096a1dafba302cfcfdc267ef280fad71b08
Christian Del Bianco <christian.delbianco@additiv.ch>
2019-03-21 07:51:37 +0100

    https://github.com/christiandelbianco/monitor-table-change-with-sqltabledependency/issues/134

---
+++ TableDependency.SqlClient.Test/StopAndStartTest.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TableDependency.SqlClient.Base.Enums;
+using TableDependency.SqlClient.Base.EventArgs;
+
+namespace TableDependency.SqlClient.Test
+{
+    [TestClass]
+    public class StopAndStartTest : Base.SqlTableDependencyBaseTest
+    {
+        private class StopAndStartTestModel
+        {
+            public long Id { get; set; }
+
+            public string Name { get; set; }            
+        }
+
+        private const string TableName = "StopAndStartTestModel";
+        private static int _counter;
+        private static Dictionary<string, Tuple<StopAndStartTestModel, StopAndStartTestModel>> _checkValues = new Dictionary<string, Tuple<StopAndStartTestModel, StopAndStartTestModel>>();
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            using (var sqlConnection = new SqlConnection(ConnectionStringForTestUser))
+            {
+                sqlConnection.Open();
+                using (var sqlCommand = sqlConnection.CreateCommand())
+                {
+                    sqlCommand.CommandText = $"IF OBJECT_ID('{TableName}', 'U') IS NOT NULL DROP TABLE [{TableName}];";
+                    sqlCommand.ExecuteNonQuery();
+
+                    sqlCommand.CommandText = $"CREATE TABLE [{TableName}]([Id] [int] IDENTITY(1, 1) NOT NULL, [Name] [NVARCHAR](50) NULL)";
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
+        }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            using (var sqlConnection = new SqlConnection(ConnectionStringForTestUser))
+            {
+                sqlConnection.Open();
+                using (var sqlCommand = sqlConnection.CreateCommand())
+                {
+                    sqlCommand.CommandText = $"DELETE FROM [{TableName}];";
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
+
+            _checkValues.Clear();
+
+            _counter = 0;
+
+            _checkValues.Add(ChangeType.Insert.ToString(), new Tuple<StopAndStartTestModel, StopAndStartTestModel>(new StopAndStartTestModel { Name = "Christian" }, new StopAndStartTestModel()));
+            _checkValues.Add(ChangeType.Update.ToString(), new Tuple<StopAndStartTestModel, StopAndStartTestModel>(new StopAndStartTestModel { Name = "Velia" }, new StopAndStartTestModel()));
+            _checkValues.Add(ChangeType.Delete.ToString(), new Tuple<StopAndStartTestModel, StopAndStartTestModel>(new StopAndStartTestModel { Name = "Velia" }, new StopAndStartTestModel()));
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            using (var sqlConnection = new SqlConnection(ConnectionStringForTestUser))
+            {
+                sqlConnection.Open();
+                using (var sqlCommand = sqlConnection.CreateCommand())
+                {
+                    sqlCommand.CommandText = $"IF OBJECT_ID('{TableName}', 'U') IS NOT NULL DROP TABLE [{TableName}];";
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
+        }
+
+        [TestCategory("SqlServer")]
+        [TestMethod]
+        public void Test()
+        {
+            SqlTableDependency<StopAndStartTestModel> tableDependency = null;
+            string naming;
+
+            try
+            {
+                tableDependency = new SqlTableDependency<StopAndStartTestModel>(ConnectionStringForTestUser, tableName: TableName);
+                tableDependency.OnChanged += TableDependency_Changed;
+                tableDependency.Start();
+                Thread.Sleep(1000 * 15 * 1);
+                tableDependency.Stop();
+                Thread.Sleep(1000 * 10 * 1);
+
+                tableDependency.Start();
+                naming = tableDependency.DataBaseObjectsNamingConvention;
+
+                var t = new Task(ModifyTableContent);
+                t.Start();
+                Thread.Sleep(1000 * 15 * 1);
+            }
+            finally
+            {
+                tableDependency?.Dispose();
+            }
+
+            Assert.AreEqual(_counter, 3);
+
+            Assert.AreEqual(_checkValues[ChangeType.Insert.ToString()].Item2.Name, _checkValues[ChangeType.Insert.ToString()].Item1.Name);
+            Assert.AreEqual(_checkValues[ChangeType.Update.ToString()].Item2.Name, _checkValues[ChangeType.Update.ToString()].Item1.Name);
+            Assert.AreEqual(_checkValues[ChangeType.Delete.ToString()].Item2.Name, _checkValues[ChangeType.Delete.ToString()].Item1.Name);
+
+            Assert.IsTrue(base.AreAllDbObjectDisposed(naming));
+            Assert.IsTrue(base.CountConversationEndpoints(naming) == 0);
+        }
+
+        private static void TableDependency_Changed(object sender, RecordChangedEventArgs<StopAndStartTestModel> e)
+        {
+            _counter++;
+
+            switch (e.ChangeType)
+            {
+                case ChangeType.Insert:
+                    _checkValues[ChangeType.Insert.ToString()].Item2.Name = e.Entity.Name;
+                    break;
+
+                case ChangeType.Update:
+                    _checkValues[ChangeType.Update.ToString()].Item2.Name = e.Entity.Name;
+                    break;
+
+                case ChangeType.Delete:
+                    _checkValues[ChangeType.Delete.ToString()].Item2.Name = e.Entity.Name;
+                    break;
+            }
+        }
+
+        private static void ModifyTableContent()
+        {
+            using (var sqlConnection = new SqlConnection(ConnectionStringForTestUser))
+            {
+                sqlConnection.Open();
+
+                using (var sqlCommand = sqlConnection.CreateCommand())
+                {
+                    sqlCommand.CommandText = $"INSERT INTO [{TableName}] ([Name]) VALUES ('{_checkValues[ChangeType.Insert.ToString()].Item1.Name}')";
+                    sqlCommand.ExecuteNonQuery();
+                }
+
+                using (var sqlCommand = sqlConnection.CreateCommand())
+                {
+                    sqlCommand.CommandText = $"UPDATE [{TableName}] SET [Name] = '{_checkValues[ChangeType.Update.ToString()].Item1.Name}'";
+                    sqlCommand.ExecuteNonQuery();
+                }
+
+                using (var sqlCommand = sqlConnection.CreateCommand())
+                {
+                    sqlCommand.CommandText = $"DELETE FROM [{TableName}]";
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
